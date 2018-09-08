e2eebbda8cb1c52647c5d1b0146698c9d8b7ea45
Junjie Huang <junjieh3@student.unimelb.edu.au>
2018-09-08 19:35:04 +1000

    identity map

--- src/onlinerealestateproject/util/IdentityMap.java
+++ src/onlinerealestateproject/util/IdentityMap.java
@@ -8,64 +8,35 @@ import onlinerealestateproject.domain.User;
 
 public class IdentityMap<E> {
 	private Map<Long, E> map = new HashMap<Long, E>();
-//	System.out.println("-------- MySQL JDBC Connection Testing ------------");
-//	User user = null;
-//	//	User user = new User(0, "harry", "potter", "harry", "123", 0, "client");
-//	IdentityMap<User> map = IdentityMap.getInstance(user);
-////	
-//	int id1 = 1;
-//	int id2 = 2;
-//	user = map.get(id1);
-////	System.out.println("1");
-//	
-//		user.setFirstName(umi.find(id1, "user").getFirstName());
-//		user.setLastName(umi.find(id1, "user").getLastName());
-//		user.setUserName(umi.find(id1, "user").getUserName());
-//		user.setPassword(umi.find(id1, "user").getPassword());
-//		user.setOrder(umi.find(id1, "user").getOrder());
-//		user.setPermission(umi.find(id1, "user").getPermission());
-//		System.out.println(user.getFirstName()+"helllo");
-//		map.put(id1, user);
-//		System.out.println(map.get(id1));
-//  }
-  public static void main(String[] argv) {
-//
-	System.out.println("-------- MySQL JDBC Connection Testing ------------");
-	User user = null;
-	IdentityMap<User> map = IdentityMap.getInstance(user);
-//	
-	int id1 = 1;
-	int id2 = 2;
-//	user = map.get(id1);
-//	System.out.println("1");
-	if (user == null) {
-		//read record from the database
-//		User user1 = new User(0, null, null, null, null, 0, null);
-		UserMapperImpl umi = new UserMapperImpl();
-		System.out.println(umi.find(1, "user").getFirstName());
 
-		
-		user.setUid(umi.find(id1, "user").getUid());
-		user.setFirstName(umi.find(id1, "user").getFirstName());
-		user.setLastName(umi.find(id1, "user").getLastName());
-		user.setUserName(umi.find(id1, "user").getUserName());
-		user.setPassword(umi.find(id1, "user").getPassword());
-		user.setOrder(umi.find(id1, "user").getOrder());
-		user.setPermission(umi.find(id1, "user").getPermission());
-//		System.out.println("test");
-//		System.out.println(user.getUid()+" hello");
-//		System.out.println(user.getFirstName()+" hello");
-		
+  public static void main(String[] argv) {
+	 int id1 = 1;
+	System.out.println("-------- MySQL JDBC Connection Testing ------------");
+	UserMapperImpl umi = new UserMapperImpl();
+	User user = new User(0, null, null, null, null, id1, null);
+	user.setUid(umi.find(id1, "user").getUid());
+	user.setFirstName(umi.find(id1, "user").getFirstName());
+	user.setLastName(umi.find(id1, "user").getLastName());
+	user.setUserName(umi.find(id1, "user").getUserName());
+	user.setPassword(umi.find(id1, "user").getPassword());
+	user.setOrder(umi.find(id1, "user").getOrder());
+	user.setPermission(umi.find(id1, "user").getPermission());
+	IdentityMap<User> map = IdentityMap.getInstance(user);
+	map.put(id1, user);
+	
+	if(user.getUid() == umi.find(id1, "user").getUid()) {
+		System.out.print("ok");
+	}else {
+		System.out.print("no ok");
 	}
-//		user.setFirstName(umi.find(id1, "user").getFirstName());
-//		user.setLastName(umi.find(id1, "user").getLastName());
-//		user.setUserName(umi.find(id1, "user").getUserName());
-//		user.setPassword(umi.find(id1, "user").getPassword());
-//		user.setOrder(umi.find(id1, "user").getOrder());
-//		user.setPermission(umi.find(id1, "user").getPermission());
-		System.out.println(user.getFirstName()+"helllo");
-		map.put(id1, user);
-		System.out.println(map.get(id1));
+
+	System.out.println(map.get(id1).uid);
+	System.out.println(map.get(id1).lastName);
+	System.out.println(map.get(id1).userName);
+	System.out.println(map.get(id1).password);
+	System.out.println(map.get(id1).getOrder());
+	System.out.println(map.get(id1).permission);
+	
   }
 	
 	private static Map<Class, IdentityMap> singletons = new HashMap<Class, IdentityMap>();

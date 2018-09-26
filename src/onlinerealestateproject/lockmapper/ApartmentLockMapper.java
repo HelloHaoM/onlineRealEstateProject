de011ac6a05e2511237fbfd56d9acb8ecffb8633
Junjie Huang <junjieh3@student.unimelb.edu.au>
2018-09-26 21:45:26 +1000

    implicit lock for apartment

---
+++ src/onlinerealestateproject/lockmapper/ApartmentLockMapper.java
@@ -0,0 +1,25 @@
+package onlinerealestateproject.lockmapper;
+
+import java.util.ArrayList;
+
+import onlinerealestateproject.datasource.DataMapperException;
+import onlinerealestateproject.domain.Apartment;
+
+public interface ApartmentLockMapper {
+
+	// find an apartment by an id
+	public Apartment find(int id);
+
+	// find all apartment
+	public ArrayList<Apartment> findAllApartments();
+
+	public boolean insert(Apartment apartment)throws DataMapperException;
+
+	public boolean update(Apartment apartment) throws DataMapperException;
+
+	public boolean delete(Apartment apartment)throws DataMapperException;
+	
+	public boolean delete(int apid) throws DataMapperException;
+	
+	
+}
